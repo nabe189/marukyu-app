2817f0e180df70685ce78cfa185a7e22f08d76b9
nabe189 <eisuke189@gmail.com>
2024-05-30 14:44:38 +0900

    修正

--- apps/buzz_reservation.py
+++ apps/buzz_reservation.py
@@ -78,7 +78,7 @@ def main():
     with col1:
         selected_date = st.date_input("日付")
     
-    time_list = ['6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30']
+    time_list = ['06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30']
     with col2:
         start_time = st.selectbox("開始時刻", time_list, index=8)
     with col3:
@@ -99,8 +99,8 @@ def main():
             table = soup.find('table', class_="studio_all_reserve_time_table")
             table_columns = ['Time'] + [div.text for div in table.find_all('div', class_="studio_reserve_time_table_studio_name")]
             reservation_state = get_reservation_state(table)
-            reservation_table = pd.DataFrame(reservation_state, columns=table_columns).set_index('Time').T[selected_time]
-            st.write(reservation_table)
+            reservation_table = pd.DataFrame(reservation_state, columns=table_columns).set_index('Time')
+            st.write(reservation_table.loc[selected_time])
             
             # 部屋のスペック
             room_names = []
